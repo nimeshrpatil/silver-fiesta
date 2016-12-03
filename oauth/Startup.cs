73c32f12b61d5bb4ab01a7b861399a015340d072
Allen <Allen@NIMESH-PC>
2016-12-02 20:55:16 -0500

    Add project files.

---
+++ oauth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(oauth.Startup))]
+namespace oauth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
